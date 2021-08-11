57db6b4c2fe5bc7d940554ff40abc90129ba8f55
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-08-11 14:28:15 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+# Nameを空のまま新規アカウントを作成させないようにしている
+# emailとpasswordに関してはdeviseの機能によってデフォルトでnull: falseがかかっているのでバリデーションを記述する必要がない
 end
