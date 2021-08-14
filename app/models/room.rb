7fe020d2bb6b9f6f183d5308bff0445ebeb48fc5
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-08-14 12:55:45 +0900

    チャットルーム作成機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,6 @@
 class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users
-# roomsテーブルはroom_usersと1対多の関係であるためこのような記述をしている
-# roomsテーブルは複数のuserの情報を持っているのでhas_manyを使っている
-# roomsテーブルとusersテーブルには中間テーブル(このテーブルを用意しないとusers,roomsテーブルに無駄なカラムが生じてしまう)があるのでthroughオプションを使ってそのように記述している
+
+  validates :name, presence: true
 end
