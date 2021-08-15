e8ace32870ed8ea52981f0fd7e5e61e464c1a56b
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-08-15 16:10:41 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,33 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
   end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    if @message.save
+    # メッセージが正しく保存された場合は以下の処理を行う
+      redirect_to room_messages_path(@room)
+    # messagesコントローラーのindexのビューを表示させる
+    # そのパスのURIには/rooms/:room_id/messagesとあるのでメッセージを投稿したroomsテーブルのidをパスに持たせて送らなければならない
+    # なのでcreateアクションの直下にroomsのidを取得して@roomに代入しているのでそれを使う
+    else
+      render :index
+    # メッセージの保存に失敗した場合はmessagesコントローラーのindexのビューを表示させる
+    # if文の処理と同じように見えるがrenderメソッドを使用しているので入力した内容は残ったままである
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+  # requireメソッドでparamsから送られるハッシュのmessageを取得している
+  # permitメソッドでcontentカラムの保存の許可をしている
+  # mergeメソッドを使って誰が(user_id)メッセージを投稿したのかを示すために記述している
+  # ログインしている人しかメッセージを投稿できないのでuser_idに対応する部分をcurrent_user.idにしている
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
-# チャットルーム一覧をトップページにしたかったのでこのようにルートパスを変更した
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
-
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
+
+# 今回messagesコントローラーにはメッセージの一覧表示(indexアクション)とメッセージの送信(createアクション)のみが必要なのでresourcesメソッドで７つのアクションを定義してonlyオプションを使うことで特定のアクションしか作動させないようにしている
+
+# messeagesコントローラーが子でroomsコントローラーが親であるようにネストをしている理由とは？
+# どのルームで投稿されたメッセージなのかをparamsを通して送られるパスから判断したいから
+# roomsテーブルのレコードのidをparamsに追加してコントローラーに送ることができる
